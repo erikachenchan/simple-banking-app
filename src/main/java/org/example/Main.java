7599e43f95118cddecb505df60736d6d9a902e41
erikachan <ahelp164@gmail.com>
2023-10-02 17:30:54 +0100

    feat: Added banking app menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,67 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String usersChoice;
+        int defaultPin = 1234;
+        boolean exit = false;
+
+        while (!exit) {
+            try {
+                System.out.println("Enter pin: ");
+                int userPin = Integer.parseInt(scanner.nextLine());
+                if (userPin == defaultPin) {
+                    displayMenu();
+                    System.out.println("Enter your choice: ");
+                    while (true) {
+                        try {
+                            usersChoice = scanner.nextLine().trim().toUpperCase();
+                            break;
+                        } catch (Exception e) {
+                            System.out.println("");
+                            System.out.println("You enter an invalid choice, please try again");
+                            System.out.println("");
+                            displayMenu();
+                        }
+                    }
+                    switch (usersChoice) {
+                        case "D":
+                            depositMoney();
+                            break;
+                        case "W":
+                            withdrawMoney();
+                            break;
+                        case "E":
+                            exit = true;
+                            break;
+                        default:
+                            System.out.println("Invalid choice, please try again");
+                            break;
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Input pin again!");
+            }
+        }
+
+    }
+
+    public static void displayMenu() {
+        System.out.println("[ W ] - Withdraw");
+        System.out.println("[ D ] - Deposit");
+        System.out.println("[ E ] - Exit");
+    }
+
+    public static double depositMoney() {
+        return 0;
+    }
+
+    public static double withdrawMoney() {
+        return 0;
     }
 }
---
+++ src/main/java/org/example/Money.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Money {
+
+    private int money;
+    private int defaultBalance;
+
+    public Money(int money) {
+        this.money = 0;
+        this.defaultBalance = 1000;
+    }
+}
