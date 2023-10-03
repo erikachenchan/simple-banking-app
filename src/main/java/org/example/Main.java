441b6a6a8441767cffded3c56555624ef894ee7e
erikachan <ahelp164@gmail.com>
2023-10-03 12:46:00 +0100

    refactor: withdraw, balance and deposit functioning now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static final Scanner scanner = new Scanner(System.in);
+    static double defaultBalance;
+    static double depositBalance;
+    static double withdrawBalance;
+    static double totalBalance;
 
     public static void main(String[] args) {
+
         String usersChoice;
         int defaultPin = 1234;
         boolean exit = false;
@@ -15,6 +20,9 @@ public class Main {
             try {
                 System.out.println("Enter pin: ");
                 int userPin = Integer.parseInt(scanner.nextLine());
+                if (userPin != defaultPin) {
+                    System.out.println("Incorrect pin!");
+                }
                 if (userPin == defaultPin) {
                     displayMenu();
                     System.out.println("Enter your choice: ");
@@ -36,6 +44,9 @@ public class Main {
                         case "W":
                             withdrawMoney();
                             break;
+                        case "C":
+                            checkBalance();
+                            break;
                         case "E":
                             exit = true;
                             break;
@@ -45,7 +56,7 @@ public class Main {
                     }
                 }
             } catch (Exception e) {
-                System.out.println("Input pin again!");
+                System.out.println("Input PIN again using four digits!");
             }
         }
 
@@ -54,14 +65,50 @@ public class Main {
     public static void displayMenu() {
         System.out.println("[ W ] - Withdraw");
         System.out.println("[ D ] - Deposit");
+        System.out.println("[ C ] - Check balance");
         System.out.println("[ E ] - Exit");
     }
 
     public static double depositMoney() {
-        return 0;
+
+        System.out.print("How much would you like to deposit: ");
+        double userInputDeposit = Double.parseDouble(scanner.nextLine());
+        depositBalance = defaultBalance + userInputDeposit;
+
+        System.out.println("Your remaining balance is: " + depositBalance + " GBP.");
+        System.out.println("Thanks for banking with us!");
+        System.out.println("");
+
+        totalBalance = depositBalance;
+        return totalBalance;
     }
 
     public static double withdrawMoney() {
-        return 0;
+
+        System.out.println("How much would you like withdraw? ");
+
+        double userInputWithdraw = Double.parseDouble(scanner.nextLine());
+
+        if (userInputWithdraw > totalBalance) {
+            System.out.println("Don't withdraw higher than your current balance!");
+        }
+        if (userInputWithdraw < totalBalance) {
+            withdrawBalance = defaultBalance - userInputWithdraw;
+        }
+        System.out.println("Your remaining balance is: " + withdrawBalance + " GBP.");
+        System.out.println("You will get " + userInputWithdraw + " GBP.");
+        System.out.println("Thanks for banking with us!");
+        System.out.println("");
+
+        totalBalance = withdrawBalance;
+        return totalBalance;
     }
+
+    private static void checkBalance() {
+
+        System.out.println("Your remaining balance is: " + totalBalance + " GBP.");
+        System.out.println("Thanks for banking with us!");
+
+    }
+
 }
