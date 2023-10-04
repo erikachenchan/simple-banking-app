7cdabbaa2c21a88c3202a1e6671be77822fa35d5
erikachan <ahelp164@gmail.com>
2023-10-04 09:55:22 +0100

    refactor: created authentication method to break authentication into a new method and avoids nesting too  much

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,56 +7,30 @@ public class Main {
     static double defaultBalance = 1000;
     static int defaultPin = 1234;
 
+    static int userPin;
+    static String usersChoice;
+
     public static void main(String[] args) {
 
-        String usersChoice;
         boolean exit = false;
-
         while (!exit) {
-            try {
-                System.out.println("Enter pin: ");
-                int userPin = Integer.parseInt(scanner.nextLine());
-
-                if (userPin == defaultPin) {
-                    displayMenu();
-                    System.out.println("Enter your choice: ");
-
-                    while (true) {
-                        try {
-                            usersChoice = scanner.nextLine().trim().toUpperCase();
-                            break;
-                        } catch (Exception e) {
-                            System.out.println("");
-                            System.out.println("You enter an invalid choice, please try again");
-                            System.out.println("");
-                            displayMenu();
-                        }
-                    }
-
-                    switch (usersChoice) {
-                        case "D":
-                            depositMoney();
-                            break;
-                        case "W":
-                            withdrawMoney();
-                            break;
-                        case "C":
-                            checkBalance();
-                            break;
-                        case "E":
-                            exit = true;
-                            break;
-                        default:
-                            System.out.println("Invalid choice, please try again");
-                            break;
-                    }
-                } else {
-                    System.out.println("Incorrect pin try again");
-                    System.out.println("");
-                }
-            } catch (Exception e) {
-                System.out.println("Input PIN again using four digits!");
-                System.out.println("");
+            authenticateUser();
+            switch (usersChoice) {
+                case "D":
+                    depositMoney();
+                    break;
+                case "W":
+                    withdrawMoney();
+                    break;
+                case "C":
+                    checkBalance();
+                    break;
+                case "E":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again");
+                    break;
             }
         }
     }
@@ -68,6 +42,37 @@ public class Main {
         System.out.println("[ E ] - Exit");
     }
 
+    public static void authenticateUser() {
+        try {
+            System.out.println("Enter pin: ");
+            userPin = Integer.parseInt(scanner.nextLine());
+
+            if (userPin == defaultPin) {
+                displayMenu();
+                System.out.println("Enter your choice: ");
+
+                while (true) {
+                    try {
+                        usersChoice = scanner.nextLine().trim().toUpperCase();
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("");
+                        System.out.println("You enter an invalid choice, please try again");
+                        System.out.println("");
+                        displayMenu();
+                    }
+                }
+            } else {
+                System.out.println("Incorrect pin try again");
+                System.out.println("");
+            }
+        } catch (
+                Exception e) {
+            System.out.println("Input PIN again using four digits!");
+            System.out.println("");
+        }
+    }
+
     public static void depositMoney() {
 
         System.out.print("How much would you like to deposit: ");
@@ -77,7 +82,6 @@ public class Main {
         System.out.println("Your remaining balance is: " + defaultBalance + " GBP.");
         System.out.println("Thanks for banking with us!");
         System.out.println("");
-
     }
 
     public static void withdrawMoney() {
@@ -100,12 +104,10 @@ public class Main {
         System.out.println("You will get " + userInputWithdraw + " GBP.");
         System.out.println("Thanks for banking with us!");
         System.out.println("");
-
     }
 
-    private static void checkBalance() {
+    public static void checkBalance() {
         System.out.println("Your remaining balance is: " + defaultBalance + " GBP.");
         System.out.println("Thanks for banking with us!");
     }
-
 }
